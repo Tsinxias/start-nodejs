7718810fa1e657fdae813e42fcb94756e61ad8f4
Zhang Tsinxias <zhangtsinxias@gmail.com>
2018-04-25 12:15:26 +0200

    minichat

--- socketio/chat.js
+++ socketio/chat.js
@@ -1,29 +1,33 @@
-const http = require('http');
-const fs = require('fs');
 const express = require('express');
-
 let app = express();
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io').listen(server);
+const ent = require('ent');
+const encode = require('ent/encode');
+const decode = require('ent/decode');
+
 
 app.use(express.static('public'));
-
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/chat.html');
 });
 
-const server = http.createServer(app);
-server.listen(8080);
-const io = require('socket.io').listen(server);
-
 io.sockets.on('connection', (socket, pseudo) => {
-    socket.on('new_pseudo', (pseudo) => {
-        socket.pseudo = pseudo;
-        console.log(socket.pseudo);
+    console.log("socket connecté");
+
+    socket.on('pseudo', (pseudo) => {
+        pseudo = ent.encode(pseudo);
+        socket.pseudo = pseudo
+        socket.broadcast.emit('new_pseudo', pseudo);
     })
 
-    socket.on('new_message', (message) => {
-        socket.message = message;
-        socket.broadcast.emit('message', {pseudo: socket.pseudo, message: socket.message})
-        console.log(socket.message);
+    socket.on('message', (data) => {
+        console.log(data);
+        socket.emit('new_message', {message: data.message, pseudo: data.pseudo});
+        socket.broadcast.emit('new_message', {message: data.message, pseudo: socket.pseudo});
     })
 })
+
+server.listen(8080);

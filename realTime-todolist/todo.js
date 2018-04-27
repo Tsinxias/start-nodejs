842e79f3f1b34b99e2ac568e841d0123cf853c91
Zhang Tsinxias <zhangtsinxias@gmail.com>
2018-04-27 15:25:45 +0200

    clean code

--- realTime-todolist/todo.js
+++ realTime-todolist/todo.js
@@ -4,29 +4,19 @@ let app = express();
 const http = require('http');
 const server = http.createServer(app);
 const io = require('socket.io').listen(server);
-// const session = require('express-session')({
-//     secret: 'hidden', 
-//     resave: true,
-//     saveUninitialized: true
-// });
-// const sharedsession = require('express-socket.io-session');
 
 let todolist = [];
 let index;
 
 app.use(express.static('public'));
-// app.use(session);
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/public/todo.html');
 })
 
-// io.use(sharedsession(session));
 
 io.sockets.on('connection', (socket) => {
     console.log('Socket connected');
-    // let sessions = socket.handshake.session;
-    // console.log(sessions);
 
     socket.emit('todolist', todolist);
 
